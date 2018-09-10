0fdad8254be92d8133e50b769061e1172d0f3bd8
Nick Essing <nick.essing@tobit.software>
2018-09-10 16:40:26 +0200

    Table output + design

--- ConsoleDatenbankausgabe/Main.cs
+++ ConsoleDatenbankausgabe/Main.cs
@@ -6,48 +6,146 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
 namespace ConsoleDatenbankausgabe
-
 {
     class main
     {
         static void Main(string[] args)
         {
-            createNewSELECT("SELECT * FROM viEmployeeAddress");
+            createNewConnection();
         }
-        public static void createNewSELECT(String SQLInput)
+        public static void createNewConnection()
         {
             try
             {
                 using (SqlConnection conn = new SqlConnection())
                 {
-                    conn.ConnectionString = "Data Source=tappqa;Initial Catalog=Training-NI-CompanyDB;Integrated Security=True";
-                    conn.Open();
-                    using (SqlDataAdapter a = new SqlDataAdapter(SQLInput, conn))
+                    try
                     {
-                        DataTable t = new DataTable();
-                        a.Fill(t);
-                        DataRow[] currentRows = t.Select(
-                        null, null, DataViewRowState.CurrentRows);
+                        conn.ConnectionString = ConsoleDatenbankausgabe.Properties.Resources.sqlConnectionString;
+                        conn.Open();
 
-                        if (currentRows.Length < 1)
-                            Console.WriteLine("No Current Rows Found");
-                        else
-                        {
-                            foreach (DataRow row in currentRows)
-                            {
-                                foreach (DataColumn column in t.Columns)
-                                    Console.Write("\t{0}", row[column]);
+                        //newStoredProcedure("spInsertOrUpdateEmployee", conn, new string[] { "@name", "@birthdate", "@salary", "@gender" }, new object[] { "nick", "2000-05-16", "3321321314", 3 });
+                        newSelect("SELECT * FROM viEmployeeAddress", conn);
 
-                                Console.WriteLine("\t");
-                            }
-                        }
+                        conn.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
                     }
                 }
             }
             catch (Exception)
             {
-                Console.WriteLine("NULL");
+                Console.WriteLine("Fehler Keine Tabelle gefunden oder verbindung möglich");
             }
+        }
+        public static void newSelect(String SQLInput, SqlConnection conn)
+        {
+            using (SqlDataAdapter a = new SqlDataAdapter(SQLInput, conn))
+            {
+                DataTable t = new DataTable();
+                a.Fill(t);
+                DataRow[] currentRows = t.Select(
+                null, null, DataViewRowState.CurrentRows);
+                List<int> paddings = getRowPaddings(t);
+
+                if (currentRows.Length < 1)
+                    Console.WriteLine("No Current Rows Found");
+                else
+                {
+                    Console.Write(" \u2554");
+                    for (int x = 0; x < paddings.Count; x++)
+                    {
+                        for (int y = 0; y < paddings[x] + 2; y++)
+                        {
+                            Console.Write("\u2550");
+                        }
+                        if (x != paddings.Count - 1)
+                        {
+                            Console.Write("\u2566");
+                        }
+                    }
+                    Console.Write("\u2557");
+                    Console.WriteLine();
+
+
+                    for (int i = 0; i < t.Columns.Count; i++){
+                        Console.Write(" \u2551 ");
+                        Console.Write(t.Columns[i].ColumnName.PadRight(paddings[i],' '));
+                    }
+                    Console.Write(" \u2551");
+                    Console.WriteLine();
+
+
+                    Console.Write(" \u2560");
+                    for (int x = 0; x < paddings.Count; x++)
+                    {
+                        for (int y = 0; y < paddings[x] + 2; y++)
+                        {
+                            Console.Write("\u2550");
+                        }
+                        if (x != paddings.Count - 1)
+                        {
+                            Console.Write("\u256C");
+                        }
+                    }
+                    Console.Write("\u2563");
+                    Console.WriteLine();
+
+
+                    for (int j = 0; j < t.Rows.Count; j++)
+                    {
+                        for (int z = 0; z < t.Columns.Count; z++)
+                        {
+                            Console.Write(" \u2551 ");
+                            Console.Write(t.Rows[j][z].ToString().PadRight(paddings[z], ' '));
+                        }
+                        Console.Write(" \u2551");
+                        Console.WriteLine();
+                    }
+
+
+                    Console.Write(" \u255A");
+                    for (int x = 0; x < paddings.Count; x++)
+                    {
+                        for (int y = 0; y < paddings[x] + 2 ;y++)
+                        {
+                            Console.Write("\u2550");
+                        }
+                        if (x != paddings.Count - 1) {
+                            Console.Write("\u2569");
+                        }
+                    }
+                    Console.Write("\u255D");
+                    Console.WriteLine();
+                }
+            }
+        }
+        public static void newStoredProcedure(String procedureName, SqlConnection conn, String[] paramName = null, object[] param = null)
+        { 
+            SqlCommand cmd = new SqlCommand(procedureName, conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            for (int i = 0; i < paramName.Length && i < param.Length; i++)
+                cmd.Parameters.AddWithValue(paramName[i],param[i]);
+            cmd.ExecuteNonQuery();
+        }
+        public static List<int> getRowPaddings(DataTable t)
+        {
+            List<int> list = new List<int>();
+            for (int j = 0; j < t.Columns.Count; j++)
+            {
+                int longestColumn = t.Columns[j].ColumnName.Length;
+                for (int i = 0; i < t.Rows.Count;i++)
+                {
+                    if(longestColumn < t.Rows[i][j].ToString().Length)
+                    {
+                        longestColumn = t.Rows[i][j].ToString().Length;
+                    }
+                }
+                list.Add(longestColumn);
+            }
+            return list;
         }
     }
 }
